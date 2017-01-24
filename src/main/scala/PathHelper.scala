1c183f6875fad2e9fea9a9ddb6252e796e204825
[a518291] Michel Daviot <michel.daviot@atos.net>
2017-01-24 18:12:36 +0100

    trivial cases

--- src/main/scala/PathHelper.scala
+++ src/main/scala/PathHelper.scala
@@ -1,7 +1,15 @@
 
-
 class PathHelper {
-  def findPath(segments: Seq[Segment], from: String, to: String) = {
-    segments.headOption
+  def findPath(segments: Seq[Segment], from: String, to: String): Option[Seq[Segment]] = {
+    if (from == to) Some(Seq())
+    else if (segments.isEmpty) None
+    else Some(segments)
   }
+
+  //      for {
+  //      s <- segments
+  //      if s.start == from
+  //      path <-
+  //    }
+
 }
--- src/test/scala/PathSpec.scala
+++ src/test/scala/PathSpec.scala
@@ -6,17 +6,27 @@ import org.scalatest.junit.JUnitRunner
 import org.scalatest.prop.PropertyChecks
 
 @RunWith(classOf[JUnitRunner])
-class GildedRoseSpec extends FlatSpec with Matchers {
+class PathSpec extends FlatSpec with Matchers {
   val helper = new PathHelper
-  val trainAB = Segment(start = "a", end = "b", mode = "train", duration = 100, distance = 80, price = 10)
+  def train(start: String, end: String) = Segment(start, end, mode = "train", duration = 100, distance = 80, price = 10)
 
   "a path helper" should "find trivial path" in {
-    val path = helper.findPath(Seq(trainAB), "a", "b")
-    path shouldBe Some(trainAB)
+    val path = helper.findPath(Seq(train("a", "b")), "a", "b")
+    path shouldBe Some(Seq(train("a", "b")))
   }
 
   it should "find no path if no segments are used" in {
     val path = helper.findPath(Seq(), "a", "b")
     path shouldBe None
+  }
+
+  it should "find an empty path if start == end " in {
+    val path = helper.findPath(Seq(), "a", "a")
+    path shouldBe Some(Seq())
+  }
+
+  it should "find a path from several segments" in {
+    val path = helper.findPath(Seq(train("a", "b"), train("a", "e"), train("c", "d"), train("b", "c")), "a", "d")
+    path shouldBe Some(Seq(train("a", "b"), train("c", "d"), train("b", "c")))
   }
 }
