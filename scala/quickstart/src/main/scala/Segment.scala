ea9becd1e66f402b1a8805a251c43890afba07ec
Michel Daviot <michel@daviot.info>
2019-05-24 16:05:33 +0200

    possible scala solution

--- scala/quickstart/src/main/scala/Segment.scala
+++ scala/quickstart/src/main/scala/Segment.scala
@@ -1,6 +1,43 @@
-case class Point(name: String, x: Long, y: Long)
+case class Point(x: Long, y: Long)
 
-case class Segment(f: Point, t: Point) {
+case class Segment(from: Point, to: Point) {
   def distance: Double =
-    math.sqrt((f.x - t.x) * (f.x - t.x) + (f.y - t.y) * (f.y - t.y))
+    math.sqrt(
+      (from.x - to.x) * (from.x - to.x) + (from.y - to.y) * (from.y - to.y)
+    )
+}
+
+case class Path(segments: List[Segment]) {
+  def stops: Set[Point] = (segments.map(_.from) ++ segments.map(_.to)).toSet
+
+  def distance: Double = segments.map(_.distance).sum
+
+  def to: Point = segments.last.to
+  def from: Point = segments.head.from
+}
+
+object Path {
+  def possiblePaths(from: Point,
+                    to: Point,
+                    segments: Set[Segment]): Set[Path] =
+    if (from == to) Set(Path(Nil))
+    else
+      for {
+        startSegment <- segments if startSegment.from == from
+        path <- possiblePaths(startSegment.to, to, segments - startSegment)
+      } yield Path(startSegment :: path.segments)
+
+  def best(points: List[Point], paths: List[Path]): Option[Path] =
+    shortest(stopAt(points, paths))
+
+  def stopAt(requiredStops: List[Point], paths: List[Path]): List[Path] =
+    paths.filter(path => requiredStops.forall(stop => path.stops(stop)))
+
+  def stopAt(requiredStop: Point, paths: List[Path]): List[Path] =
+    paths.filter(_.stops(requiredStop))
+
+  def shortest(paths: List[Path]): Option[Path] =
+    if (paths.nonEmpty)
+      Some(paths.minBy(_.distance))
+    else None
 }
--- scala/quickstart/src/test/scala/PathSpec.scala
+++ scala/quickstart/src/test/scala/PathSpec.scala
@@ -1,18 +1,20 @@
+import Path._
 import org.scalatest.{FlatSpec, Matchers}
 
 class PathSpec extends FlatSpec with Matchers {
 
-  val pa = Point("a", 0, 0)
-  val pb = Point("b", 10, 10)
-  val pc = Point("c", 20, 0)
-  val pd = Point("d", 30, 0)
-  val pe = Point("e", 40, 0)
+  val pa = Point(x = 0, y = 0)
+  val pb = Point(x = 10, y = 10)
+  val pc = Point(x = 20, y = 0)
+  val pd = Point(x = 30, y = 0)
+  val pe = Point(x = 40, y = 0)
+  val pf = Point(x = 40, y = 20)
 
-  val ac = Segment(pa, pc)
-  val bc = Segment(pb, pc)
-  val ab = Segment(pa, pb)
-  val cd = Segment(pc, pd)
-  val de = Segment(pd, pe)
+  val ac = Segment(from = pa, to = pc)
+  val bc = Segment(from = pb, to = pc)
+  val ab = Segment(from = pa, to = pb)
+  val cd = Segment(from = pc, to = pd)
+  val de = Segment(from = pd, to = pe)
   // a---c--d--e
   //  \ /
   //   b
@@ -20,6 +22,42 @@ class PathSpec extends FlatSpec with Matchers {
   "a segment" should "compute its distance" in {
     val distance = ac.distance
     distance shouldBe 20.0 +- 0.0001
+  }
+
+  val acde = Path(List(ac, cd, de))
+  "a path" should "compute its distance" in {
+    acde.distance shouldBe 40.0 +- 0.0001
+  }
+
+  it should "list where it stops" in {
+    acde.stops shouldBe Set(pa, pc, pd, pe)
+  }
+
+  val abcde = Path(List(ab, bc, cd, de))
+
+  "paths" should "find the shortest path" in {
+    shortest(List(acde, abcde)) shouldBe Some(acde)
+  }
+
+  it should "filter on a given stop" in {
+    stopAt(pb, List(acde, abcde)) shouldBe List(abcde)
+    stopAt(pa, List(acde, abcde)) shouldBe List(acde, abcde)
+    stopAt(pf, List(acde, abcde)) shouldBe Nil
+  }
+
+  it should "filter on several stops" in {
+    stopAt(List(pa, pb), List(acde, abcde)) shouldBe List(abcde)
+    stopAt(List(pf), List(acde, abcde)) shouldBe Nil
+  }
+
+  it should "find the best path" in {
+    best(List(pa, pb), List(acde, abcde)) shouldBe Some(abcde)
+  }
+
+  it should "list possible paths" in {
+    possiblePaths(pa, pb, Set()) shouldBe Set()
+    possiblePaths(pa, pb, Set(ab)) shouldBe Set(Path(List(ab)))
+    possiblePaths(pa, pe, Set(ac, cd, de, ab, bc)) shouldBe Set(Path(List(ac, cd, de)), Path(List(ab, bc, cd, de)))
   }
 
 }
